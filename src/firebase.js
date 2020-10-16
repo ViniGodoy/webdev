9fde5a59380b842962b5f18aa0a947bf025efcc7
Vinícius Godoy de Mendonça <vinigodoy@gmail.com>
2020-10-16 20:55:11 -0300

    Removed firebase credentials.

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC_h3vSw6vwoQCRYDQhkHWXfKTrbu1zbqQ",
-    authDomain: "reactblog-12e56.firebaseapp.com",
-    databaseURL: "https://reactblog-12e56.firebaseio.com",
-    projectId: "reactblog-12e56",
-    storageBucket: "reactblog-12e56.appspot.com",
-    messagingSenderId: "377438658779",
-    appId: "1:377438658779:web:0d66a17bd4693a7da5ecfe"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 };
 
 // Initialize Firebase
